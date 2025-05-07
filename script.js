053002a07602d8b4f90eda5a2d82a344792a78bd
Pranay Chandurkar <pranaychandurkar84@gmail.com>
2025-05-07 21:47:28 +0530

    change

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add fade class to all elements on the page
+window.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("*").forEach(el => {
+      el.classList.add("fade");
+    });
+  });
+  
